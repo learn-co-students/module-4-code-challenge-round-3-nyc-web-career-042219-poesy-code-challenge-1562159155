837698bdeafc85724d8dffe4c5be606b0cbdf2d9
Herman Rivas <hrivas5726@aol.com>
2019-07-03 11:05:22 -0400

    success

--- src/Poem.js
+++ src/Poem.js
@@ -3,6 +3,7 @@ import React from 'react';
 class Poem extends React.Component {
   render(){
     return (
+      //working on color change
       <div onClick={this.props.changeColor}   style={!this.props.colorText? {color: "black"} :{color: "red"}}>
         <h3>{this.props.poem.title}</h3>
         <p>{this.props.poem.content}</p>
